c115122819a009698bc211515897ddc5f84cb57b
Eduardo Garcia <egarcia@Eduardos-MacBook-Air.local>
2024-01-27 21:12:44 -0500

    Final version imersion with new folder React

---
+++ React/spotify/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Sidebar from './Sidebar/Sidebar';
+import Header from './Header/Header';
+import Main from './Main/Main';
+import Footer from './Footer/Footer';
+
+
+function App() {
+  return (
+    <div>
+      <Sidebar/>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
